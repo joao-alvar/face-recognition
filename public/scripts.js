859f8372bfabaf027f7168736f4303585baba800
João Victor Alvarenga <victor.alvar@tutanota.com>
2024-06-19 11:37:16 -0300

    Add update facial detection with user browser permision.

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,24 @@
-console.log(faceapi)
+console.log(faceapi);
 
-const run = async()=>{
-    //we need to load our models
-    //loading the models is going to use await
+const run = async () => {
+  //we need to load our models
+  //loading the models is going to use await
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+  const videoFeedEl = document.getElementById("video-feed");
+  videoFeedEl.srcObject = stream;
 
-}
+  //we need to load the models
+  // pre-trained machine learning for the facial detection!
+  await Promise.all([
+    faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.ageGenderNet.loadFromUri("./models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+  ]);
+};
 
-run()
+run();
